41ba8cde43a39030aeda64487c69d55ce5b29c81
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-31 18:38:11 +0530

    NavBar2 component created and completed

--- src/Components/ViewAllComplaint.jsx
+++ src/Components/ViewAllComplaint.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import NavBar2 from './NavBar2'
 
 const ViewAllComplaint = () => {
     const [compData, setCompData] = useState(
@@ -20,6 +21,7 @@ const ViewAllComplaint = () => {
 
     return (
         <div>
+            <NavBar2/>
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
