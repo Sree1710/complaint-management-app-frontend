d020bfa219b409e4fd792ca8dad7224fd446f598
Sree1710 <sreelekshmisl1710@gmail.com>
2023-11-09 14:04:39 +0530

    Modifications Done

--- src/Components/ViewAllComplaint.jsx
+++ src/Components/ViewAllComplaint.jsx
@@ -19,13 +19,13 @@ const ViewAllComplaint = () => {
     }
 
 
-    
+
 
     useEffect(() => { getData() }, [])
 
     return (
         <div>
-            <NavBar2/>
+            <NavBar2 />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -33,24 +33,38 @@ const ViewAllComplaint = () => {
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <h1>View All Complaints</h1>
                             </div>
-                            {compData.map(
-                                (value, index) => {
-                                    return <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4 d-flex align-items-stretch">
-                                        <div class="card">
-                                            <div class="card-body">
-                                                <h5 class="card-title">Complaint ID: {value.compId}</h5>
-                                                <p class="card-text"><h5>Title: </h5>{value.compTitle}</p>
-                                                <p class="card-text"><h5>Description: </h5>{value.compDesc}</p>
-                                                <p class="card-text"><h5>Location: </h5>{value.compLocation}</p>
-                                                <p class="card-text"><h5>Complaint Posted Date: </h5>{value.compDate}</p>
-                                                <p class="card-text"><h5>Remarks: </h5>{value.Remarks}</p>
-                                                {value.Status==0 ? <p class="card-text"><h5>Status: </h5>Processing</p> : <p class="card-text"><h5>Status: </h5>Issue Resolved</p> }
-                                                <a href={'/addr/'+value._id}  class="btn btn-primary">Add Remarks</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                }
-                            )}
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <table className="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Complaint ID</th>
+                                            <th scope="col">Title</th>
+                                            <th scope="col">Description</th>
+                                            <th scope="col">Location</th>
+                                            <th scope="col">Complaint Posted Date</th>
+                                            <th scope="col">Remarks</th>
+                                            <th scope="col">Status</th>
+                                            <th scope="col"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {compData.map(
+                                            (value, index) => {
+                                                return <tr>
+                                                    <th scope="row">{value.compId}</th>
+                                                    <td>{value.compTitle}</td>
+                                                    <td>{value.compDesc}</td>
+                                                    <td>{value.compLocation}</td>
+                                                    <td>{value.compDate}</td>
+                                                    <td>{value.Remarks}</td>
+                                                    {value.Status == 0 ? <td>Processing</td> : <td>Issue Resolved</td>}
+                                                    <td><a href={'/addr/' + value._id} className="btn btn-primary">Add Remarks</a></td>
+                                                </tr>
+                                            }
+                                        )}
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
